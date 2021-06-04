bdc1c18d858d665d55de28dce2ad92a3f6ef2486
salohiddin1221 <salohiddinkhayitov0@gmail.com>
2021-06-04 17:09:23 +0500

    fixed bug

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
 
 
+
 setInterval(() => {
     let day = new Date();
-    let hh = day.getHours()* 30;
-    let mm = day.getMinutes() *deg;
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
     let ss = day.getSeconds() * deg;
-    
-    hr.style.transform = `rotate( ${(hh) + (mm / 12)} deg)`;
-    mn.style.transform = `rotate(${(mm)}deg)`;
-    sc.style.transform= `rotate(${(ss)}deg)`
-}, 1000);
+
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+});
